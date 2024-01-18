c292817b65d10f5ef74ceed26dfc7b702e18a877
Joshua Coleman <jncoleman123@gmail.com>
2024-01-17 20:38:01 -0500

    Create newpractice.hs

---
+++ newpractice.hs
@@ -0,0 +1,11 @@
+-- newpractice.hs
+module NewPractice where
+
+firstString :: String 
+firstString = "Something is afoot."
+
+secondString :: String 
+secondString = "No it is now "
+
+main :: IO ()
+    print "Something is happening"
